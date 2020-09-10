91713e81ed3d85c55b929b8fa479b870f4a6efd3
Ray <ray9015@gmail.com>
2020-09-10 15:35:59 +0800

    Modify encrypt string for merchant request.

--- lib/spgateway_rails/merchant/spgateway_merchant.rb
+++ lib/spgateway_rails/merchant/spgateway_merchant.rb
@@ -7,6 +7,11 @@ module SpgatewayRails
       SERVICE_TYPE = 'API'
 			SERVICE_ACTION = 'AddMerchant'
 
+      def get_encrypt_string
+        encode_param = URI.encode_www_form(@post_data)
+        SpgatewayRails::SpgatewayHelper.encrypt_merchant_data encode_param
+      end
+
       def get_post_data
         post_data = {
           PartnerID_: SpgatewayRails.merchant_config.partner_id,
